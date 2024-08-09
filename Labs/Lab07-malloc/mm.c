b860775d333daad0d4f74f788a0b31d8165bee2f
fjc-hub <1661007260@qq.com>
2024-08-09 23:52:13 +0800

    0.accomplish Lab07 malloc-lab:      Perf index = 45 ( space utilization) + 40 (throughput) = 85/100 1.refine MIN_FREE_BLOCK_SZ to 32 bytes,  a minimum block must have space to store two Pointers, a header, and a footer 2.implements mm_realloc()

--- Labs/Lab07-malloc/mm.c
+++ Labs/Lab07-malloc/mm.c
@@ -43,6 +43,8 @@ team_t team = {
 
 #define MIN_FREE_BLOCK_SZ 32 // 2 * ALIGNMENT + 2 * SIZE_T_SIZE; minimun size of free block
 
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 // get value pointed to by void pointer
 #define GET(p) (*(unsigned long *)(p))
 // set val to the location pointed to by void pointer
@@ -56,6 +58,10 @@ team_t team = {
 
 void *HEADER; // head block base-address of the heap
 void *TAILER; // tail block base-address of the heap
+
+int mm_check_free_list(); // check function
+void rm_free_node_from_list(void *ptr);  // remove a free block node from implicit free list
+void insert_free_node_into_list(void *ptr); // insert a free block node into implicit free list, inserted node is next to HEADER
 
 /* 
  * mm_init - initialize the malloc package.
@@ -77,13 +83,12 @@ int mm_init(void)
     size_t tail_blk_sz = ALIGN(SIZE_T_SIZE + 2 * ALIGNMENT);
     void *tailer_start = VOID_DEL(last, tail_blk_sz - 1);
     PUT(VOID_DEL(last, ALIGNMENT - 1), 0); //init tailer node's next pointer
-    PUT(tailer_start, 0x1); // init tailer node's size to 0
+    PUT(tailer_start, 0x3); // init tailer node's size to 0, and prev block is allocated
 
-    size_t head_blk_sz = ALIGN(2 * ALIGNMENT + 2 * SIZE_T_SIZE);
     PUT(first, 0x1); // init header node's size to 0 and alloc-control-bit to 1 
     PUT(VOID_ADD(first, SIZE_T_SIZE), 0); // init header node's prev pointer
     PUT(VOID_ADD(first, ALIGNMENT + SIZE_T_SIZE), tailer_start); // init header node's next pointer
-    PUT(VOID_ADD(first, ALIGNMENT + 2 * SIZE_T_SIZE), head_blk_sz); // init header node's footer
+    PUT(VOID_ADD(first, ALIGNMENT + 2 * SIZE_T_SIZE), 0x1); // init header node's footer
    
     PUT(VOID_ADD(tailer_start, SIZE_T_SIZE), first); // init tailer node's prev node pointer
     PUT(VOID_ADD(first, ALIGNMENT + SIZE_T_SIZE), tailer_start); // init header node's next node pointer
@@ -116,13 +121,13 @@ void *mm_malloc(size_t size)
     }
 
     // adjust size to align ALIGNMENT bytes
-    size_t resize = ALIGN(size + SIZE_T_SIZE); // allocated block has no footer, only header
+    size_t resize = MAX(MIN_FREE_BLOCK_SZ, ALIGN(size + SIZE_T_SIZE)); // allocated block has no footer, only header
 
     // look through explicit free list
     void *curr = HEADER;
     while(curr != NULL) {
         size_t curr_sz = GET_SIZE(curr);
-        if (resize < curr_sz) {
+        if (resize <= curr_sz) {
             // match
             // split curr free block if necessary
             if (resize + MIN_FREE_BLOCK_SZ <= curr_sz) { // split it and use lower-bit part as allocated block 
@@ -130,7 +135,7 @@ void *mm_malloc(size_t size)
                 void *prev_node_ptr = (void *) GET((void *) VOID_ADD(curr, SIZE_T_SIZE)); // get previous free node pointer
                 void *next_node_ptr = (void *) GET((void *) VOID_ADD(curr, SIZE_T_SIZE + ALIGNMENT)); // get next free node pointer
                 void *old_ptr = (void *) VOID_ADD(curr, resize); // use second part as old free block
-                PUT(old_ptr, (curr_sz - resize) | 0x2); // set block size and control-bit that determines if byte subsequent block is allocated
+                PUT(old_ptr, (curr_sz - resize) | 0x2); // set header
                 PUT(VOID_ADD(old_ptr, SIZE_T_SIZE), prev_node_ptr); // set previous free node pointer
                 PUT(VOID_ADD(old_ptr, SIZE_T_SIZE + ALIGNMENT), next_node_ptr); // set next free node pointer
                 PUT(VOID_ADD(old_ptr, curr_sz - resize - SIZE_T_SIZE), (curr_sz - resize) | 0x2); // set footer
@@ -138,31 +143,19 @@ void *mm_malloc(size_t size)
                 PUT(VOID_ADD(next_node_ptr, SIZE_T_SIZE), old_ptr); // set next-free-block's prev pointer 
 
                 // 2.construct allocated block
-                unsigned long old_sz_bit_mask = (GET(curr) & 0x7); // get old 3 control bits
+                unsigned long old_sz_bit_mask = (GET(curr) & 0x6); // get old 2 control bits
                 PUT(curr, (resize | old_sz_bit_mask) | 0x1); // set header's size and remain old control-bit
 
                 // 3.return the base address of payload 
                 return (void *) VOID_ADD(curr, SIZE_T_SIZE);
             } else { // not split and use whole block as allocated block (剩余的空闲块大小不满足MIN_FREE_BLOCK_SZ条件，存不下指向前后空闲块的指针)
-                // 1.set next block's second control-bit to 0x1
+                // 1.update header
                 void *next_block_ptr = (void *) VOID_ADD(curr, curr_sz);
-                PUT(next_block_ptr, GET(next_block_ptr) | 0x2); // update header
-
-                // check if next_block_ptr block is allocated or free
-                // unsigned long phy_next_blk_bits = GET(next_block_ptr) & 0x7;
-                // if ((phy_next_blk_bits & 0x1) > 0) { // next_block_ptr block is allocated, no need to update footer
-                //     PUT(next_block_ptr, GET(next_block_ptr) | 0x2); // update header
-                // } else { // next_block_ptr block is free
-                //     PUT(next_block_ptr, GET(next_block_ptr) | 0x2); // update header
-                //     size_t next_blk_sz = GET_SIZE(next_block_ptr);
-                //     PUT(VOID_ADD(next_block_ptr, next_blk_sz - SIZE_T_SIZE), GET_SIZE(next_block_ptr) | 0x2); // update footer
-                // }
+                PUT(next_block_ptr, GET(next_block_ptr) | 0x2); // update next block's header
+                PUT(curr, GET(curr) | 0x1); // update this block's header
 
                 // 2.remove curr block from implicit free list
-                void *prev_node_ptr = (void *) GET(VOID_ADD(curr, SIZE_T_SIZE));
-                void *next_node_ptr = (void *) GET(VOID_ADD(curr, SIZE_T_SIZE + ALIGNMENT)); 
-                PUT(VOID_ADD(prev_node_ptr, SIZE_T_SIZE + ALIGNMENT), (unsigned long) next_node_ptr);
-                PUT(VOID_ADD(next_node_ptr, SIZE_T_SIZE), (unsigned long) prev_node_ptr);
+                rm_free_node_from_list(curr);
 
                 // 3.return the base address of payload 
                 return  (void *) VOID_ADD(curr, SIZE_T_SIZE);
@@ -189,9 +182,9 @@ void *mm_malloc(size_t size)
         void *new_tail = VOID_DEL(last, ALIGN(SIZE_T_SIZE + 2 * ALIGNMENT) - 1);
         void *tailer_prev_node_ptr = (void *) GET((void *) VOID_ADD(tailer, SIZE_T_SIZE)); // tailer block's prev free node
 
-        PUT(new_tail, GET(tailer) | 0x2); // set new tailer block's control-bit and size
-        PUT(VOID_ADD(new_tail, SIZE_T_SIZE), GET(VOID_ADD(tailer, SIZE_T_SIZE))); // set new tailer block's prev-pointer
-        PUT(VOID_ADD(new_tail, SIZE_T_SIZE + ALIGNMENT), GET(VOID_ADD(tailer, SIZE_T_SIZE + ALIGNMENT))); // set new tailer block's next-pointer
+        PUT(new_tail, GET(tailer) | 0x3); // set new tailer block's control-bit and size
+        PUT(VOID_ADD(new_tail, SIZE_T_SIZE), tailer_prev_node_ptr); // set new tailer block's prev-pointer
+        PUT(VOID_ADD(new_tail, SIZE_T_SIZE + ALIGNMENT), NULL); // set new tailer block's next-pointer
 
         // 2.construct allocated block
         unsigned long new_control_bit = GET(tailer) & 0x7;
@@ -201,7 +194,6 @@ void *mm_malloc(size_t size)
         // 3.update TAILER
         TAILER = new_tail;
 
-// printf("0x%lx 0x%lx\n", (unsigned long) HEADER, (unsigned long) tailer_prev_node_ptr);
         // 4.update previous free block (of tailer) 's next node pointer
         PUT(VOID_ADD(tailer_prev_node_ptr, SIZE_T_SIZE + ALIGNMENT), new_tail);
 
@@ -215,14 +207,16 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    // mm_check_free_list();
+
     if (ptr == NULL) 
         return;
     
     void *block_ptr = VOID_DEL(ptr, SIZE_T_SIZE); // block pointer
     unsigned long pre_blk_alloc = GET(block_ptr) & 0x2; // flag presenting if physically previous block is allocated 
-    unsigned long block_sz = GET(block_ptr) & ~0x7; // block size
+    unsigned long block_sz = GET_SIZE(block_ptr); // block size
     void *next_block_ptr = (void *) VOID_ADD(block_ptr, block_sz); // next block's pointer
-    unsigned long nxt_blk_alloc = GET(next_block_ptr) & 0x2; // flag presenting if physically next block is allocated
+    unsigned long nxt_blk_alloc = GET(next_block_ptr) & 0x1; // flag presenting if physically next block is allocated
 
     // check if block ptr can be coalesced and merge consecutive free blocks
     void *new_block_ptr = block_ptr;
@@ -230,19 +224,11 @@ void mm_free(void *ptr)
     if (pre_blk_alloc) {
         if (nxt_blk_alloc) {
             // case 1: prev block and next block are both allocated
-            // insert new_block_ptr to the implicit free list
-            void *head_ptr = HEADER;
-            void *head_next_node_ptr = (void *) GET(VOID_ADD(head_ptr, SIZE_T_SIZE + ALIGNMENT));
-
-            PUT(VOID_ADD(head_ptr, SIZE_T_SIZE + ALIGNMENT), (unsigned long) new_block_ptr); // set header block's next node
-            PUT(VOID_ADD(head_next_node_ptr, SIZE_T_SIZE), (unsigned long) new_block_ptr); // set prev node
-            
-            PUT(VOID_ADD(new_block_ptr, SIZE_T_SIZE), (unsigned long) head_ptr); // set prev node of new free block
-            PUT(VOID_ADD(new_block_ptr, SIZE_T_SIZE + ALIGNMENT), (unsigned long) head_next_node_ptr); // set next node of new free block
             PUT(new_block_ptr, new_block_size | 0x2); // set header of new free block
             PUT(VOID_ADD(new_block_ptr, new_block_size-SIZE_T_SIZE), new_block_size | 0x2); // set footer of new free block
-
             PUT(next_block_ptr, GET(next_block_ptr) & ~0x2); // set next block's control bit, cause its prev block is free
+
+            insert_free_node_into_list(new_block_ptr); // insert new_block_ptr to the implicit free list
         } else {
             // case 2: prev block is allocated, next block is free
             void *nxt_prev_node_ptr = (void *) GET(VOID_ADD(next_block_ptr, SIZE_T_SIZE)); // get next block's prev node
@@ -268,9 +254,7 @@ void mm_free(void *ptr)
             new_block_ptr = prev_block_ptr; // change block pointer
             new_block_size += GET_SIZE(prev_block_ptr);
 
-            unsigned long prev_block_control_bit = GET(prev_block_ptr) & 0x7; // get previous block's address
-            unsigned long nxt_block_control_bit = GET(next_block_ptr) & 0x7; // get next block's address
-
+            unsigned long prev_block_control_bit = GET(prev_block_ptr) & 0x6; // get previous block's address
             PUT(prev_block_ptr, new_block_size | prev_block_control_bit); // update new block's header
             PUT(new_block_footer, new_block_size | prev_block_control_bit); // update new block's footer
 
@@ -280,15 +264,19 @@ void mm_free(void *ptr)
             void *prev_block_footer = (void *) VOID_DEL(new_block_ptr, SIZE_T_SIZE);
             void *prev_block_ptr = (void *) VOID_DEL(new_block_ptr, GET_SIZE(prev_block_footer)); // previous block's address
             void *next_block_ptr = (void *) VOID_ADD(new_block_ptr, new_block_size); // next block's address
-            void *next_block_footer = (void *) VOID_DEL(next_block_ptr, GET_SIZE(next_block_ptr) - SIZE_T_SIZE); // new block's footer
+            void *next_block_footer = (void *) VOID_ADD(next_block_ptr, GET_SIZE(next_block_ptr) - SIZE_T_SIZE); // new block's footer
+
+            rm_free_node_from_list(prev_block_ptr); // remove free block node
+            rm_free_node_from_list(next_block_ptr); // remove free block node
 
             new_block_ptr = prev_block_ptr; // change block pointer
             new_block_size += (GET_SIZE(prev_block_ptr) + GET_SIZE(next_block_ptr));
 
-            unsigned long prev_block_control_bit = GET(prev_block_ptr) & 0x7; // get previous block's address
-
+            unsigned long prev_block_control_bit = GET(prev_block_ptr) & 0x6; // get previous block's address
             PUT(prev_block_ptr, new_block_size | prev_block_control_bit); // update new block's header
             PUT(next_block_footer, new_block_size | prev_block_control_bit); // update new block's footer
+
+            insert_free_node_into_list(new_block_ptr); // insert into free list
         }
     }
 
@@ -301,15 +289,80 @@ void *mm_realloc(void *ptr, size_t size)
 {
     void *oldptr = ptr;
     void *newptr;
-    size_t copySize;
     
+    if (size == 0) {
+        mm_free(ptr);
+        return (void *) 0;
+    }
+
+    if (oldptr == NULL) {
+        return mm_malloc(size);
+    }
+    
+    // check if the ptr block has enough payload for size
+    size_t new_sz = MAX(MIN_FREE_BLOCK_SZ, ALIGN(size + SIZE_T_SIZE));
+    size_t old_sz = GET_SIZE((void *) VOID_DEL(ptr, SIZE_T_SIZE));
+    if (old_sz >= new_sz) {
+        return ptr;
+    }
+    
+    // get a new free block 
     newptr = mm_malloc(size);
     if (newptr == NULL)
       return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
+    
+    memcpy(newptr, oldptr, old_sz);
     mm_free(oldptr);
     return newptr;
+}
+
+void rm_free_node_from_list(void *ptr) {
+    void *prev_node_ptr = (void *) GET(VOID_ADD(ptr, SIZE_T_SIZE));
+    void *next_node_ptr = (void *) GET(VOID_ADD(ptr, SIZE_T_SIZE + ALIGNMENT));
+
+    PUT(VOID_ADD(prev_node_ptr, SIZE_T_SIZE + ALIGNMENT), (unsigned long) next_node_ptr);
+    PUT(VOID_ADD(next_node_ptr, SIZE_T_SIZE), (unsigned long) prev_node_ptr);
+}
+
+void insert_free_node_into_list(void *ptr) {
+    void *head_ptr = HEADER;
+    void *head_next_node_ptr = (void *) GET(VOID_ADD(head_ptr, SIZE_T_SIZE + ALIGNMENT));
+
+    PUT(VOID_ADD(head_ptr, SIZE_T_SIZE + ALIGNMENT), (unsigned long) ptr); // set header block's next node
+    PUT(VOID_ADD(head_next_node_ptr, SIZE_T_SIZE), (unsigned long) ptr); // set prev node
+    
+    PUT(VOID_ADD(ptr, SIZE_T_SIZE), (unsigned long) head_ptr); // set prev node of new free block
+    PUT(VOID_ADD(ptr, SIZE_T_SIZE + ALIGNMENT), (unsigned long) head_next_node_ptr); // set next node of new free block
+}
+
+int mm_check_free_list() {
+    void *curr = HEADER;
+    curr = (void *) GET(VOID_ADD(curr, SIZE_T_SIZE + ALIGNMENT)); // skip HEADER
+    while(curr != NULL && curr != TAILER) {
+        size_t sz = GET_SIZE(curr);
+        unsigned long is_alloc = GET(curr) & 0x1;
+        if (sz > 0 && is_alloc) {
+            printf("non-free node in implicit free list\n");
+            exit(-1);
+        }
+        unsigned long is_pre_alloc = GET(curr) & 0x2;
+        if (!is_pre_alloc) {
+            unsigned long pre_sz = GET_SIZE(VOID_DEL(curr, SIZE_T_SIZE));
+            if (pre_sz > 0) {
+                printf("free block's prev block is free and not coalesced\n");
+                return -1;
+            }
+        }
+        unsigned long is_nxt_alloc = GET(VOID_ADD(curr, sz)) &0x1;
+        if (!is_nxt_alloc) {
+            unsigned long nxt_sz = GET_SIZE(VOID_ADD(curr, sz));
+            if (nxt_sz > 0) {
+                printf("free block's next block is free and not coalesced\n");
+                exit(-1);
+            }
+        }
+        // next node
+        curr = (void *) GET(VOID_ADD(curr, SIZE_T_SIZE + ALIGNMENT));
+    }
+    return -1;
 }
